faf19ecc76c44137ea6ebe094c661a7c89ba26b1
Eduardo Camillo <eduardo.ckf@gmail.com>
2025-03-29 11:04:31 -0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxthub/core'
+  ],
+
+  future: {
+    compatibilityVersion: 4,
+  },
+})
