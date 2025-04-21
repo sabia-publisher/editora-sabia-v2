c3cf37bdebc67a91502287692cbfaf7c3cbe6433
Eduardo Camillo <eduardo.ckf@gmail.com>
2025-04-21 10:48:01 -0300

    feat: hello world (deploy inicial)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hello Word</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxthub/core'
-  ],
+	modules: [
+		'@nuxt/content',
+		'@nuxt/eslint',
+		'@nuxt/fonts',
+		'@nuxt/image',
+		'@nuxt/scripts',
+		'@nuxthub/core'
+	],
 
-  future: {
-    compatibilityVersion: 4,
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	future: {
+		compatibilityVersion: 4,
+	},
 })
