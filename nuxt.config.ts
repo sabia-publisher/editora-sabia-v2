9c9033888f085a4ed8a1708de5f2784efbb91fae
Eduardo Camillo <eduardo.ckf@gmail.com>
2025-07-05 18:27:14 -0300

    feat: adding nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxthub/core'
+  ],
+  
+  content: {
+    preview: {
+      api: 'https://sabia-publisher.nuxt.dev'
+    }
+  },
+  
   compatibilityDate: '2025-05-15',
-  
   devtools: { enabled: true },
-
-  modules: ['@nuxt/content', '@nuxthub/core'],
-  
   future: {
     compatibilityVersion: 4,
   },
